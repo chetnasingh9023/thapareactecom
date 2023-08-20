3d2650bd1b2b59fa15984efbf76bcf2253affb07
Chetna Singh <chetnasingh7488@gmail.com>
2023-08-20 12:41:39 +0530

    react project2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+import Products from "./Products";
+import Contact from "./Contact";
+import SingleProduct from "./SingleProduct";
+import Cart from "./Cart";
 const App = () => {
-  return <div>Welcome to Thapa React E-Commerce Website</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Products" element={<Products />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/SingleProduct" element={<SingleProduct />} />
+        <Route path="/Cart" element={<Cart />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
